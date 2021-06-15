48fe492f15e3c4a7a7952d83a750fb6583bd81b4
sophie-wjyang <sophie.wjyang@gmail.com>
2021-06-15 12:26:15 -0400

    getting started

---
+++ memorygame/src/memorygame/Frame.java
@@ -0,0 +1,13 @@
+package memorygame;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class Frame {
+	public static void main(String[] args) {
+		JFrame aFrame = new JFrame("Harry Styles");
+		aFrame.setSize(800,800);
+		aFrame.setVisible(true);
+		aFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+	}
+}
