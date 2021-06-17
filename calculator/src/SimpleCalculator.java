6d07001b15e58690b81bf05229646ca350e52ac3
sophie-wjyang <sophie.wjyang@gmail.com>
2021-06-17 16:40:01 -0400

    new version

--- calculator/src/SimpleCalculator.java
+++ calculator/src/SimpleCalculator.java
@@ -1,5 +1,3 @@
-package calculator;
-
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
@@ -8,7 +6,27 @@ class SimpleCalculator extends JFrame implements ActionListener{
 	
 	//creating display
 	private JTextField output = new JTextField(20);
-	private JPanel panel;
+	String equation = "";
+	
+	//creating panels
+	JPanel panel1 = new JPanel();
+	JPanel panel2 = new JPanel();
+	JPanel panel3 = new JPanel();
+	JPanel panel4 = new JPanel();
+	JPanel panel5 = new JPanel();
+	JPanel panel6 = new JPanel();
+	JPanel panel7 = new JPanel();
+	JPanel panel8 = new JPanel();
+	JPanel panel9 = new JPanel();
+	JPanel panel0 = new JPanel();
+	JPanel panelAdd = new JPanel();
+	JPanel panelSubtract = new JPanel();
+	JPanel panelMultiply = new JPanel();
+	JPanel panelDivide = new JPanel();
+	JPanel panelDecimal = new JPanel();
+	JPanel panelNegative = new JPanel();
+	JPanel panelCalculate = new JPanel();
+	JPanel panelClear = new JPanel();
 	
 	//creating buttons
 	JButton oneButton = new JButton("1"); 
@@ -34,26 +52,7 @@ class SimpleCalculator extends JFrame implements ActionListener{
 	public SimpleCalculator(String n) {
 		super(n);
 		
-		JPanel panel1= new JPanel();
-		JPanel panel2= new JPanel();
-		JPanel panel3= new JPanel();
-		JPanel panel4= new JPanel();
-		JPanel panel5= new JPanel();
-		JPanel panel6= new JPanel();
-		JPanel panel7= new JPanel();
-		JPanel panel8= new JPanel();
-		JPanel panel9= new JPanel();
-		JPanel panel0= new JPanel();
-		JPanel panelAdd= new JPanel();
-		JPanel panelSubtract= new JPanel();
-		JPanel panelMultiply= new JPanel();
-		JPanel panelDivide= new JPanel();
-		JPanel panelDecimal= new JPanel();
-		JPanel panelNegative= new JPanel();
-		JPanel panelCalculate= new JPanel();
-		JPanel panelClear= new JPanel();
-		
-		
+		//creating container
         Container c = getContentPane();
         //getContentPane().setBackground(Color.gray);
         c.setLayout(null);
@@ -70,204 +69,213 @@ class SimpleCalculator extends JFrame implements ActionListener{
         this.add(panel1);
         oneButton.setSize(80,80);
         oneButton.setLocation(50,140);
-        //addButton.addActionListener(this);
-        
        
         c.add(twoButton);
         twoButton.addActionListener(this);
         this.add(panel2);
         twoButton.setSize(80,80);
         twoButton.setLocation(160,140);
-        //addButton.addActionListener(this);
         
         c.add(threeButton);
         threeButton.addActionListener(this);
         this.add(panel3);
         threeButton.setSize(80,80);
         threeButton.setLocation(270,140);
-        //addButton.addActionListener(this);
         
         c.add(addButton);
         addButton.addActionListener(this);
         this.add(panelAdd);
         addButton.setSize(80,80);
         addButton.setLocation(380,140);
-        //addButton.addActionListener(this);
-
+       
         c.add(fourButton);
         fourButton.addActionListener(this);
         this.add(panel4);
         fourButton.setSize(80,80);
         fourButton.setLocation(50,250);
-        //subtractButton.addActionListener(this);
         
         c.add(fiveButton);
         fiveButton.addActionListener(this);
         this.add(panel5);
         fiveButton.setSize(80,80);
         fiveButton.setLocation(160,250);
-        //subtractButton.addActionListener(this);
         
         c.add(sixButton);
         sixButton.addActionListener(this);
         this.add(panel6);
         sixButton.setSize(80,80);
         sixButton.setLocation(270,250);
-        //subtractButton.addActionListener(this);
         
         c.add(subtractButton);
         subtractButton.addActionListener(this);
         this.add(panelSubtract);
         subtractButton.setSize(80,80);
         subtractButton.setLocation(380,250);
-        //subtractButton.addActionListener(this);
         
         c.add(sevenButton);
         sevenButton.addActionListener(this);
         this.add(panel7);
         sevenButton.setSize(80,80);
         sevenButton.setLocation(50,360);
-        //multiplyButton.addActionListener(this);
         
         c.add(eightButton);
         eightButton.addActionListener(this);
         this.add(panel8);
         eightButton.setSize(80,80);
         eightButton.setLocation(160,360);
-        //multiplyButton.addActionListener(this);
         
         c.add(nineButton);
         nineButton.addActionListener(this);
         this.add(panel9);
         nineButton.setSize(80,80);
         nineButton.setLocation(270,360);
-        //multiplyButton.addActionListener(this);
         
         c.add(multiplyButton);
         multiplyButton.addActionListener(this);
         this.add(panelMultiply);
         multiplyButton.setSize(80,80);
         multiplyButton.setLocation(380,360);
-        //multiplyButton.addActionListener(this);
         
         c.add(zeroButton);
         zeroButton.addActionListener(this);
         this.add(panel0);
         zeroButton.setSize(80,80);
         zeroButton.setLocation(50,470);
-        //multiplyButton.addActionListener(this);
         
         c.add(decimalButton);
         decimalButton.addActionListener(this);
         this.add(panelDecimal);
         decimalButton.setSize(80,80);
         decimalButton.setLocation(160,470);
-        //multiplyButton.addActionListener(this);
         
         c.add(negativeButton);
         negativeButton.addActionListener(this);
         this.add(panelNegative);
         negativeButton.setSize(80,80);
         negativeButton.setLocation(270,470);
-        //multiplyButton.addActionListener(this);
         
         c.add(divideButton);
         divideButton.addActionListener(this);
         this.add(panelDivide);
         divideButton.setSize(80,80);
         divideButton.setLocation(380,470);
-        //divideButton.addActionListener(this);
-     
-        c.add(calculateButton);
-        calculateButton.addActionListener(this);
-        this.add(panelCalculate);
-        calculateButton.setSize(190,50);
-        calculateButton.setLocation(50,580);
-        //divideButton.addActionListener(this);
         
         c.add(clearButton);
         clearButton.addActionListener(this);
         this.add(panelClear);
         clearButton.setSize(190,50);
         clearButton.setLocation(270,580);
-        //divideButton.addActionListener(this);
+        
+        c.add(calculateButton);
+        calculateButton.addActionListener(this);
+        output.setText(equation);
+        this.add(panelCalculate);
+        calculateButton.setSize(190,50);
+        calculateButton.setLocation(50,580);
+        output.setText(equation);
+        
+     
       	    
 	}
 	
 	public void actionPerformed(ActionEvent e) {
-		if (e.getSource() == oneButton)
-		{
-			output.setText(output.getText().concat(String.valueOf(1)));
-		}
-		else if (e.getSource() == twoButton)
-		{
-			output.setText(output.getText().concat("2"));
-		}
-		else if (e.getSource() == threeButton)
-		{
-			output.setText(output.getText().concat(String.valueOf(3)));
-		}
-		else if (e.getSource() == fourButton)
-		{
-			output.setText(output.getText().concat("4"));
-		}
-		else if (e.getSource() == fiveButton)
-		{
-			output.setText(output.getText().concat("5"));
-		}
-		else if (e.getSource() == sixButton)
-		{
-			output.setText(output.getText().concat("6"));
-		}
-		else if (e.getSource() == sevenButton)
-		{
-			output.setText(output.getText().concat("7"));
-		}
-		else if (e.getSource() == eightButton)
-		{
-			output.setText(output.getText().concat("8"));
-		}
-		else if (e.getSource() == nineButton)
-		{
-			output.setText(output.getText().concat("9"));
-		}
-		else if (e.getSource() == zeroButton)
-		{
-			output.setText(output.getText().concat("0"));
-		}
-		else if (e.getSource() == addButton)
-		{
-			output.setText("");
-		}
-		else if (e.getSource() == subtractButton)
-		{
-			output.setText("");
-		}
-		else if (e.getSource() == multiplyButton)
-		{
-			output.setText("");
-		}
-		else if (e.getSource() == divideButton)
-		{
-			output.setText("");
-		}
-		else if (e.getSource() == decimalButton)
-		{
-			output.setText(output.getText().concat("."));
-		}
-		else if (e.getSource() == negativeButton)
-		{
-			output.setText(output.getText().concat("-"));
-		}
-		else if (e.getSource() == clearButton)
-		{
-			output.setText(""); //need to edit
-		}
-		else if (e.getSource() == calculateButton)
-		{
-			output.setText(output.getText().concat("")); //need to edit
-		}
 		
+		
+		
+		//recognizing which button was clicked, displaying it, and adding it to a string
+		//while(e.getSource() != calculateButton) {
+			if (e.getSource() == oneButton)
+			{
+				equation += "1";
+				output.setText(equation);
+			}
+			else if (e.getSource() == twoButton)
+			{
+				equation += "2";
+				output.setText(equation);
+			}
+			else if (e.getSource() == threeButton)
+			{
+				equation += "3";
+				output.setText(equation);
+			}
+			else if (e.getSource() == fourButton)
+			{
+				equation += "4";
+				output.setText(equation);
+			}
+			else if (e.getSource() == fiveButton)
+			{
+				equation += "5";
+				output.setText(equation);
+			}
+			else if (e.getSource() == sixButton)
+			{
+				equation += "6";
+				output.setText(equation);
+			}
+			else if (e.getSource() == sevenButton)
+			{
+				equation += "7";
+				output.setText(equation);
+			}
+			else if (e.getSource() == eightButton)
+			{
+				equation += "8";
+				output.setText(equation);
+			}
+			else if (e.getSource() == nineButton)
+			{
+				equation += "9";
+				output.setText(equation);
+			}
+			else if (e.getSource() == zeroButton)
+			{
+				equation += "0";
+				output.setText(equation);
+			}
+			else if (e.getSource() == addButton)
+			{
+				equation += "+";
+				output.setText(equation);
+			}
+			else if (e.getSource() == subtractButton)
+			{
+				equation += "-";
+				output.setText(equation);
+			}
+			else if (e.getSource() == multiplyButton)
+			{
+				equation += "*";
+				output.setText(equation);
+			}
+			else if (e.getSource() == divideButton)
+			{
+				equation += "/";
+				output.setText(equation);
+			}
+			else if (e.getSource() == decimalButton)
+			{
+				equation += ".";
+				output.setText(equation);
+			}
+			else if (e.getSource() == negativeButton)
+			{
+				equation += "_"; //underscore
+				output.setText(equation);
+			}
+			else if (e.getSource() == clearButton)
+			{
+				equation = "";
+				output.setText("");
+			}
+			else if (e.getSource() == calculateButton)
+			{
+				output.setText(equation);
+			}
+			
+		//}
+	
 		
 	}
 	
